0873e436cd183ba0685056e047c6f21903dcf516
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-20 19:28:45 -0300

    Clase N°11: Sobrecarga de funciones: la solución.

--- src/08-overload.ts
+++ src/08-overload.ts
@@ -1,7 +1,7 @@
 // Galeed -> ['G', 'a', 'l', 'e', 'e', 'd']
 //['G', 'a', 'l', 'e', 'e', 'd'] -> Galeed
 
-function parseStr (input: string | string[]): string | string[] {
+export function parseStr (input: string | string[]): string | string[] {
     if (Array.isArray(input)) return input.join(''); // string
     return input.split(''); // string[]
 }
