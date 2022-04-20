a9f91efdd6a5eeb36ecc0aa8ad6a524e5d4a5ded
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-20 17:34:06 -0300

    Clase N°8: Parámetros por defecto V2.

--- src/05-optionals.ts
+++ src/05-optionals.ts
@@ -1,18 +1,16 @@
 export const createProduct = (
     id: string | number,
-    isNew: boolean = true,
-    stock: number = 10
+    isNew?: boolean,
+    stock?: number
 ) => {
     return {
         id,
-        isNew,
-        stock
+        isNew: isNew ?? true,
+        stock: stock ?? 10
     }
 }
 
 const p1 = createProduct(1, true, 12);
-const p2 = createProduct(2);
-const p3 = createProduct(3, false, 0);
-const p4 = createProduct(4, true, 100);
-const p5 = createProduct(5, false);
-console.log({p1, p2, p3, p4, p5});
+const p2 = createProduct(1);
+const p3 = createProduct(1, false, 0);
+console.log({p1, p2, p3});
--- src/06-default.ts
+++ src/06-default.ts
@@ -1,16 +1,18 @@
 export const createProduct = (
     id: string | number,
-    isNew?: boolean,
-    stock?: number
+    isNew: boolean = true,
+    stock: number = 10
 ) => {
     return {
         id,
-        isNew: isNew ?? true,
-        stock: stock ?? 10
+        isNew,
+        stock
     }
 }
 
 const p1 = createProduct(1, true, 12);
-const p2 = createProduct(1);
-const p3 = createProduct(1, false, 0);
-console.log({p1, p2, p3});
+const p2 = createProduct(2);
+const p3 = createProduct(3, false, 0);
+const p4 = createProduct(4, true, 100);
+const p5 = createProduct(5, false);
+console.log({p1, p2, p3, p4, p5});
