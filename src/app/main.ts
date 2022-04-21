ec481891a32fb8257f510272b19977d28c7b62ff
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-21 06:00:17 -0300

    Clase N°16: Lógica del CRUD terminada.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,8 @@
-import { addProduct, products } from './products/product.service';
+import { addProduct, products, updateProduct, readProduct, deleteProduct } from './products/product.service';
 import { faker } from '@faker-js/faker';
+import { Product } from './products/product.model';
 
-for (let index = 0; index < 50; index++) {
+for (let index = 0; index < 2; index++) {
     addProduct({
         id: faker.datatype.uuid(),
         description: faker.commerce.productDescription(),
@@ -23,5 +24,73 @@ for (let index = 0; index < 50; index++) {
         }
     });
 }
+addProduct({
+    id: 123,
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    size: faker.random.arrayElement(['S', 'M', 'L', 'XL']),
+    price: parseInt(faker.commerce.price()),
+    isNew: faker.datatype.boolean(),
+    tags: faker.random.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
+    category: {
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent()
+    }
+});
 
-console.log({products});
+addProduct({
+    id: 465,
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    size: faker.random.arrayElement(['S', 'M', 'L', 'XL']),
+    price: parseInt(faker.commerce.price()),
+    isNew: faker.datatype.boolean(),
+    tags: faker.random.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
+    category: {
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent()
+    }
+});
+
+const changes: Product = {
+    id: 'changes',
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    size: faker.random.arrayElement(['S', 'M', 'L', 'XL']),
+    price: parseInt(faker.commerce.price()),
+    isNew: faker.datatype.boolean(),
+    tags: faker.random.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
+    category: {
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent()
+    }
+}
+
+updateProduct(123, changes, products);
+readProduct('changes', products);
+deleteProduct('changes', products);
+readProduct('changes', products);
+updateProduct(123, changes, products);
+deleteProduct('changes', products);
+// console.log({products});
